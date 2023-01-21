7ac7652a5b052a7108019eafa031bac473416e93
Bence Piatrik <69726557+bencepiatrik@users.noreply.github.com>
2023-01-21 23:55:00 +0100

    Update dbs_outprint_servlet.java

--- dbs_outprint_servlet.java
+++ dbs_outprint_servlet.java
@@ -6,7 +6,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
-import java.sql.Statement;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
